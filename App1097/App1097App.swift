12b721e2c2cd4b98dae937115c2f5357ab8fbb1d
WebShell1998 <43918790+WebShell1998@users.noreply.github.com>
2024-11-08 06:11:51 +0300

    Initial Commit

---
+++ App1097/App1097App.swift
@@ -0,0 +1,17 @@
+//
+//  App1097App.swift
+//  App1097
+//
+//  Created by Николай Щербаков on 08.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct App1097App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
